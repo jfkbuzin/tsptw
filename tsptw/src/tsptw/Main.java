a2f97d03882fa2f98690dc9a0aaeb92cc763eb19
root <root@asusadm-Z450LA>
2017-11-26 18:16:33 -0200

    test

--- tsptw/src/tsptw/Main.java
+++ tsptw/src/tsptw/Main.java
@@ -1,10 +1,45 @@
 package tsptw;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		ArrayList<Vertice> vertices =  new ArrayList<Vertice>();
+		ArrayList<Edge> edges =  new ArrayList<Edge>();
+		
+		/*
+		Scanner s = new Scanner(new File("n10w140.txt"));
+		ArrayList<String> ids = new ArrayList<String>();
 
+		// Skip column headings.
+
+		// Read each line, ensuring correct format.
+		while (s.hasNext())
+		{
+			ids.add(s.next());
+			s.nextInt();         // read and skip 'id'
+			s.nextInt();  // read and store 'name'
+		    s.nextInt();
+		    s.nextInt(); 
+		    s.nextInt(); 
+		    s.nextInt(); // read and skip 'age'
+		}
+
+		for (String id: ids)
+		{
+		    System.out.println(id);
+		}*/
+		
+		System.out.println("ok");
+		
+		Graph graph = new Graph(vertices,edges);
+		
+		
 	}
 
 }
